63bf9b1872bcb45d0cc4d81ea1e716b8686f1fc1
Ethan Smith <esmith164@mail.valenciacollege.edu>
2023-11-03 09:29:25 -0400

    Add navbar

---
+++ face-id-dashboard/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="navbar-component">
+    <h4 class="font-medium text-blue-600">Node ICU</h4>
+    <ul class="navbar-nav">
+      <li><router-link to="/">Dashboard</router-link></li>
+      <li><router-link to="/">Settings</router-link></li>
+      <li class="button"><router-link to="/">Add User</router-link></li>
+    </ul>
+  </div>
+</template>
+<style lang="scss" scoped>
+.navbar-component {
+  @apply border-b border-slate-300 flex justify-between items-center px-4 py-4 shadow-sm;
+  .navbar-nav {
+    @apply flex;
+    li {
+      @apply mx-2;
+      a {
+        @apply text-slate-500 hover:text-slate-500 duration-200;
+        &:hover {
+          @apply text-orange-500;
+        }
+      }
+    }
+  }
+}
+</style>
--- face-id-dashboard/src/views/Home.vue
+++ face-id-dashboard/src/views/Home.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+</script>
 <template>
   <div class="dashboard-landing">
+    <Navbar />
     Example!
   </div>
 </template>
