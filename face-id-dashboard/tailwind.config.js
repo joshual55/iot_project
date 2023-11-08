3499ba8ae0e47c14d91b0643a0da029667f848fe
Ethan Smith <esmith164@mail.valenciacollege.edu>
2023-11-08 05:02:16 -0500

    Potential fix

--- face-id-dashboard/tailwind.config.js
+++ face-id-dashboard/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue,html}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,html}'],
+
   theme: {
     extend: {},
   },
