ad39c35a535c2aa18e406686c84cd28224b63b9d
Ethan Smith <esmith164@mail.valenciacollege.edu>
2023-11-08 04:46:11 -0500

    Allow JS build

--- face-id-dashboard/vite.config.ts
+++ face-id-dashboard/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     proxy: {
       // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
       "/api": {
-        target: "https://9otaictrgg.execute-api.us-east-2.amazonaws.com/my-add-face-deployment",
+        target: "https://k9nqp1eh2c.execute-api.us-east-2.amazonaws.com/add-face",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       }, 
