317139273659012bbc66837ff79063df9ce620e9
Ethan Smith <esmith164@mail.valenciacollege.edu>
2023-11-08 08:44:27 -0500

    ADd netlify redirects

---
+++ face-id-dashboard/src/components/AccessLog.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { supabase } from '@/supabase'
+import { ref } from 'vue';
+const entryLogs = ref([]);
+const fetchEntryLogs = async () => {
+  const { data, error } = await supabase
+  .from('entry_history')
+  .select();
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+    entryLogs.value = data;
+  }
+};
+fetchEntryLogs()
+</script>
+<template>
+  <div class="access-log-component container mx-auto">
+    <div class="user-header my-6">
+      <h2 class="text-2xl font-medium">Entry History</h2>
+      <p>Access to door lock history</p>
+    </div>
+    <div class="grid grid-cols-5 border-b py-3">
+      <div>
+        <h4>Entry Time</h4>
+      </div>
+      <div>
+        <h4>User ID</h4>
+      </div>
+      <div>
+        <h4>Unlocked</h4>
+      </div>
+      <div>
+        <h4>Two Factor Success</h4>
+      </div>
+      <div>
+        <h4>Face Recognized</h4>
+      </div>
+    </div>
+  </div>
+</template>
--- face-id-dashboard/src/views/Home.vue
+++ face-id-dashboard/src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import AccessLog from  '@/components/AccessLog.vue';
 import Users from '@/components/Users.vue';
 </script>
 <template>
   <div class="dashboard-landing">
     <Navbar />
-    <Users />
+    <Users class="mb-14"/>
+    <AccessLog />
   </div>
 </template>
